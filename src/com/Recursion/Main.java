adc2eb5118d34ab2534213662ada9d15674b4bc1
Nitin <nsinghboss02@gmail.com>
2022-02-05 14:38:21 +0530

    printing the actual paths.

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args){
         // count the number of paths.
         System.out.println(count(3,3));
-
+        paths("",3,3);
     }
     public static int count(int r,int c){
         if(r==1 || c==1){                         // base condition is r==1 or c==1 because if any of them becomes
@@ -15,5 +15,18 @@ public class Main {
         int left = count(r-1,c);
         int right = count(r,c-1);
         return left+right;
+    }
+    // Now return the actual paths too.
+    public static void paths(String p,int r,int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            paths(p+'D',r-1,c);
+        }
+        if(c>1){
+            paths(p+'R',r,c-1);
+        }
     }
 }
